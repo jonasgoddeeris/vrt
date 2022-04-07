16d29bbacf37f0b19032103736cea9ea10a17dae
Jonas G <goddeerisjonas@me.com>
2022-04-07 15:03:24 +0200

    validation

--- scripts-google-sheet/onEdit-multirowdynamicvalidation.js
+++ scripts-google-sheet/onEdit-multirowdynamicvalidation.js
@@ -1,17 +1,17 @@
 function onEdit() {
   // the current sheet and cell 
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  var current = sheet.getActiveCell()
+  var cell = sheet.getActiveCell()
   
   // var to refer to the worksheets
   //var vrtnu = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("vrtNU"); (GENERIEK GEMAAKT)
   var list = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sources");
   
   // which medium has the user selected (based on active cell if column number is 4)?
-  if (current.getColumn()==4)
+  if (cell.getColumn()==4 && cell.getRow() > 7)
     {
     //copy the selected medium from any (utm) file  
-      var medium = current.getValue()
+      var medium = cell.getValue()
     //into the sources file as a dynamic indirect generator
       list.getRange(2,12).setValue(medium)
         
@@ -19,17 +19,18 @@ function onEdit() {
     sheet.getRange(8,5,468).clearDataValidations()
       
     // set the indirect validation to the cell right to it.
-    var point = current.offset(0,1)
+    var point = cell.offset(0,1)
 
     //get the values in the indrect column of the "Sources" sheet, get values H2 to H50. (H = the 11 colomn)
     var sources = list.getRange(2,11,50)
 
     //build a dataValdiation rule based on sources mathcing the selected medium
-    var rule = SpreadsheetApp.newDataValidation().requireValueInRange(sources,true).build();
+    var dv = SpreadsheetApp.newDataValidation().requireValueInRange(sources,true).build();
     
     // clear content
     point.clearContent();
-    point.setDataValidation(rule)
+    point.setDataValidation(dv);
+    dv.setHelpText("Selecteer een Source (Bron). De geselecteerde source moet aansluiten bij het gekozen medium.");
 
     }
 }
